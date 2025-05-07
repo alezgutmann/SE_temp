a1ae05ee1546dd0f2a41ef6c9a6718d6654c9a75
git <carlo-morais@web.de>
2025-05-07 14:13:20 +0200

    hilfe

---
+++ MiarbeiterSoftware/src/main/Controller.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Controller {
+	
+	public Controller() {
+		
+	}
+	
+	
+}
---
+++ MiarbeiterSoftware/src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setTitle("MitarbeiterSoftware");
+		window.setSize(500, 500);
+		
+		JPanel panel = new JPanel();
+		
+		
+		
+		window.setVisible(true);
+	}
+
+}
