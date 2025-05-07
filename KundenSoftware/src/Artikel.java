3b134f08e02e6bd6c53a3cd6ca8edcd19d612324
alezgutmann <supersalat090505@gmail.com>
2025-05-07 14:34:42 +0200

    Artikel funktioniert, javadoc initiiert

---
+++ KundenSoftware/src/Artikel.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.JSONObject;
+
+/**
+ * Die Klasse {@code Artikel} repräsentiert einen Artikel mit Preis, Artikelnummer, Name und Vorschaubild.
+ * Sie kann einen Artikel aus einer JSON-Datei laden.
+ */
+public class Artikel{
+	private double preis;
+	private String artikelNR;
+	private String name;
+	private String vorschaubild;
+	
+	public Artikel() {
+	}
+	
+	 /**
+     * Konstruktor zum Erstellen eines Artikels mit allen Eigenschaften.
+     * Wird allerdings so nicht zum initialisieren verwendet, 
+     * da er in artikelladen() enthalten ist.
+     *
+     * @param preis         Der Preis des Artikels
+     * @param artikelNR     Die Artikelnummer
+     * @param name          Der Name des Artikels
+     * @param vorschaubild  Der Pfad oder Name des Vorschaubilds
+     */
+	public Artikel(double preis, String artikelNR,String name, String vorschaubild) {
+		this.preis = preis;
+		this.artikelNR = artikelNR;
+		this.name = name;
+		this.vorschaubild = vorschaubild;
+	}
+	
+	 /**
+     * Lädt einen Artikel aus einer JSON-Datei.
+     *
+     * @param pfad Pfad zur JSON-Datei, die die Artikeldaten enthält
+     * @return Ein {@code Artikel}-Objekt mit den geladenen Daten
+     * @throws IOException Wenn ein Fehler beim Lesen der Datei auftritt
+     */
+	public static Artikel artikelladen(String pfad) throws IOException{
+		String inhalt = new String(Files.readAllBytes(Paths.get(pfad)));
+		JSONObject json = new JSONObject(inhalt);
+		Artikel artikel = new Artikel(json.getDouble("preis"), json.getString("artikelNR"), json.getString("name"), json.getString("vorschaubild"));
+		return artikel;
+	}
+	
+	/**
+     * Gibt eine String-Repräsentation des Artikels zurück.
+     *
+     * @return String mit Artikelinformationen
+     */
+	@Override
+	public String toString() {
+		return "Artikel{ " + "preis=" + preis + ", artikelNR=" + artikelNR + ", name=" + name + ", vorschaubild=" + vorschaubild + " }";
+	}
+
+	/**
+     * Hauptmethode zum Testen des Ladevorgangs eines Artikels aus einer JSON.
+     *
+     * @param args Kommandozeilenargumente werden nicht verwendet
+     */
+	public static void main(String[] args) {
+		try {
+			Artikel artikel = Artikel.artikelladen("./Essen/burger.json");
+			System.out.println(artikel);
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
