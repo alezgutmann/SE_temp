b755eb4412dc397170f4f341a6c039eaccec22ab
git <carlo-morais@web.de>
2025-05-07 13:58:28 +0200

    mitarbeiterschmeiter

---
+++ KundenSoftware/src/main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Easy Brew");
+	}
+
+}
