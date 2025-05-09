29e1284dfb202d05dd67387358149bd244acbaac
alezgutmann <supersalat090505@gmail.com>
2025-05-09 10:55:31 +0200

    Update Artikel.java

--- KundenSoftware/src/Artikel.java
+++ KundenSoftware/src/Artikel.java
@@ -79,15 +79,6 @@ public class Artikel {
 
 		return null;
 	}
-=======
-public class Artikel {
-    private double preis;
-    private String artikelNR;
-    private String name;
-    private String vorschaubild;
->>>>>>> refs/heads/Max
-
-<<<<<<< HEAD
 	/**
 	 * Gibt eine String-Repräsentation des Artikels zurück.
 	 *
@@ -117,45 +108,4 @@ public class Artikel {
 		return this.preis;
 	}
 }
-=======
-    public Artikel() {}
-    
-    public Artikel(double preis, String artikelNR, String name, String vorschaubild) {
-        this.preis = preis;
-        this.artikelNR = artikelNR;
-        this.name = name;
-        this.vorschaubild = vorschaubild;
-    }
-    //nimmt Werte aus Json Datei
-    public static Artikel artikelladen(String pfad) throws IOException {
-        String inhalt = new String(Files.readAllBytes(Paths.get(pfad)));
-        JSONObject json = new JSONObject(inhalt);
-        return new Artikel(
-            json.getDouble("preis"),
-            json.getString("artikelNR"),
-            json.getString("name"),
-            json.getString("vorschaubild")
-        );
-    }
-    
-    //gibt Namen an Menu zurueck
-    public String getName() {
-        return name;
-    }
-    //gibt Preis an Menu zurueck
-    public Double getPreis() {
-    	return preis;
-    }
-    //gibt Bild an Menu zurueck
-    public String getVorschaubild() {
-        return vorschaubild;
-    }
 
-    
-    @Override
-    public String toString() {
-        return "Artikel{ " + "preis=" + preis + ", artikelNR=" + artikelNR +
-               ", name=" + name + ", vorschaubild=" + vorschaubild + " }";
-    }
-}
->>>>>>> refs/heads/Max
